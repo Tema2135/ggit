04b86ec736a11caa99de6116a25404689fe94074
Tema2135 <temaperne@gmail.com>
2025-03-28 15:45:02 +0500

    m

---
+++ src/main/java/org/example/ggit/GgitApplication.java
@@ -0,0 +1,13 @@
+package org.example.ggit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgitApplication.class, args);
+    }
+
+}
